f3de7e866f38e0d7e04b9fa2656541ed00301e78
Ravizj <ravizj57@gmail.com>
2022-10-14 13:47:23 +0530

    this is commit

---
+++ src/LineComperisoin/UC_2.java
@@ -0,0 +1,20 @@
+package LineComperisoin;
+
+import java.util.Scanner;
+
+public class UC_2 {
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to Line\r\n"
+                    + "Comparison Computation");
+            System.out.println("Enter the length of line");
+            Scanner sc =new Scanner(System.in);
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            if(x==y) {
+                System.out.println(x+" X equal to Y "+y);
+            }
+            else
+                System.out.println(x+" X is not equal to Y  "+y);
+        }
+    }
